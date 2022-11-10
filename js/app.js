844742ba7022176b007023033a412157f26520c5
Kishor Mahmud <dev.kishor138@gmail.com>
2022-11-10 22:32:10 +0600

    ready fro javascript

--- js/app.js
+++ js/app.js
@@ -1,4 +1,30 @@
+players = [];
+function displayPlayerName(players){
+    const ul = document.querySelector('ul');
+    
+    ul.textContent = '';
+
+    for(const player of players){
+        const playerList = document.createElement('ol');
+        playerList.textContent = player;
+        ul.appendChild(playerList);
+    }
+    
+}
+
 function addPlayerName(player){
     const playerName = player.parentNode.children[0].innerText;
-    console.log(playerName);
+    players.push(playerName);
+    displayPlayerName(players);
+
 }
+
+function getInputValue(id){
+    const inputField = document.getElementById(id);
+    const inputValue = parseFloat(inputField.value);
+    return inputValue;
+}
+document.getElementById('calculate-button').addEventListener('click', function(){
+    const perPlayer = getInputValue('per-player');
+    
+})
